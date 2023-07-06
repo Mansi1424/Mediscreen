76ef83e4f333bdefcd1da8456460f4c6de8bb6d7
Mansi1424 <mansi2001.edu@gmail.com>
2023-07-05 21:10:28 -0400

    Creation of repository

---
+++ src/test/java/com/mansi/mediscreen/mediscreen/MediscreenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mansi.mediscreen.mediscreen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediscreenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
